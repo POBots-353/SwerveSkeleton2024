7e8b88efc74ed9ce1efd4cbe04302f4b043403f3
NandaGuntupalli <70027620+NandaGuntupalli@users.noreply.github.com>
2024-05-19 09:47:59 -0400

    adjusted setState methods for swerve modules

--- src/main/java/frc/robot/subsystems/Swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveModule.java
@@ -46,10 +46,6 @@ public class SwerveModule extends SubsystemBase {
     setAngle(desiredState);
   }
 
-  public void setVoltage() {
-    
-  }
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
